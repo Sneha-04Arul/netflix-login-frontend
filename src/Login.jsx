dfc37b870224734bba87c753d7e301b991bdde38
A Sneha <sneha04arul@gmail.com>
2026-01-08 13:08:27 +0530

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,32 @@
 import React from "react";
-import axios from "axios";
+import "./Login.css";
 
 function Login() {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="login-container">
+      <div className="login-box">
+        <h1>Sign In</h1>
+
+        <input type="email" placeholder="Email or phone number" />
+        <input type="password" placeholder="Password" />
+
+        <button>Sign In</button>
+
+        <div className="options">
+          <label>
+            <input type="checkbox" /> Remember me
+          </label>
+          <span>Need help?</span>
+        </div>
+
+        <p className="signup">
+          New to Netflix? <span>Sign up now.</span>
+        </p>
+
+        <p className="captcha">
+          This page is protected by Google reCAPTCHA to ensure you're not a bot.
+        </p>
+      </div>
     </div>
   );
 }
